cd2d06ce48ca038003180a4f11ed75691d3e64f6
Rohit Jagga <rohitjagga02@gmail.com>
2022-04-30 16:38:00 -0400

    Update redditParser.java

--- project-code/src/redditParser.java
+++ project-code/src/redditParser.java
@@ -66,47 +66,77 @@ public class redditParser {
     }
 
     public void getCapsPosts() {
-        String capsURL = "https://old.reddit.com/r/UPenn/search?q=caps&restrict_sr=on&include_over_18=on&sort=relevance&t=all";
-        try {
-            this.currentDoc = Jsoup.connect(capsURL).get();
-            // System.out.println(this.currentDoc);
-        } catch (IOException e) {
-            //System.out.println("Could not connect to link");
-        }
-        Elements postElems = this.currentDoc.select("a[href*=https://old.reddit.com/r/UPenn/comments/]");
-        for (Element post : postElems) {
-            String postURL = post.attr("href");
-            if (this.capsPostLinks.size() == 0) {
-                this.capsPostLinks.add(postURL);
-            } else if (!this.capsPostLinks.getLast().equals(postURL)) {
-                this.capsPostLinks.add(postURL);
+        String nextPageLink = "https://old.reddit.com/r/UPenn/search?q=caps&restrict_sr=on&include_over_18=on&sort=relevance&t=all";
+        boolean firstTime = true;
+        while (nextPageLink != null) {
+            try {
+                this.currentDoc = Jsoup.connect(nextPageLink).get();
+//                System.out.println("went to next page");
+            } catch (IOException e) {
+                return;
+            }
+            Elements postElems = this.currentDoc.select("a[href*=https://old.reddit.com/r/UPenn/comments/]");
+            for (Element post : postElems) {
+                String postURL = post.attr("href");
+                if (this.capsPostLinks.size() == 0) {
+                    this.capsPostLinks.add(postURL);
+                } else if (!this.capsPostLinks.getLast().equals(postURL)) {
+                    this.capsPostLinks.add(postURL);
+                }
+            }
+
+            Elements nextButton = this.currentDoc.getElementsByClass("nextprev");
+            if (nextButton.size() == 0) {
+                nextPageLink = null;
+            } else {
+                Elements buttonATag = nextButton.first().select("a");
+                if (buttonATag.size() == 2) {
+                    nextPageLink = buttonATag.get(buttonATag.size()-1).attr("href");
+                } else if (firstTime) {
+                    nextPageLink = buttonATag.get(0).attr("href");
+                    firstTime = false;
+                } else {
+                    nextPageLink = null;
+                }
             }
         }
 
         System.out.println(this.capsPostLinks);
+        System.out.println(this.capsPostLinks.size());
     }
 
     public void getAcademicPosts() {
-        String url = "https://old.reddit.com/r/UPenn/new/";
         this.academicPostLinks = new LinkedList<String>();
-        try {
-            this.currentDoc = Jsoup.connect(url).get();
-            // System.out.println(this.currentDoc);
-        } catch (IOException e) {
-            //System.out.println("Could not connect to link");
-        }
-        Elements aTags = this.currentDoc.select("span:contains(Academic/Career)");
-        for (Element aTag : aTags) {
-            Element postTag = aTag.parent().parent().select("a").first();
-            String postLink = postTag.attr("href");
-            String nextPostURL = "https://old.reddit.com" + postLink;
-            if (this.academicPostLinks.size() == 0 || !nextPostURL.equals(this.academicPostLinks.getLast())) {
-                this.academicPostLinks.add(nextPostURL);
+        String nextPageLink = "https://old.reddit.com/r/UPenn/new/";
+        while (nextPageLink != null) {
+            try {
+                this.currentDoc = Jsoup.connect(nextPageLink).get();
+                // System.out.println(this.currentDoc);
+//                System.out.println("went to next page");
+            } catch (IOException e) {
+                //System.out.println("Could not connect to link");
+                return;
+            }
+            Elements aTags = this.currentDoc.select("span:contains(Academic/Career)");
+            for (Element aTag : aTags) {
+                Element postTag = aTag.parent().parent().select("a").first();
+                String postLink = postTag.attr("href");
+                String nextPostURL = "https://old.reddit.com" + postLink;
+                if (this.academicPostLinks.size() == 0 || !nextPostURL.equals(this.academicPostLinks.getLast())) {
+                    this.academicPostLinks.add(nextPostURL);
+                }
+            }
+
+            Elements nextButton = this.currentDoc.getElementsByClass("next-button");
+            if (nextButton.size() == 0) {
+                nextPageLink = null;
+            } else {
+                Elements buttonATag = nextButton.first().select("a");
+                nextPageLink = buttonATag.first().attr("href");
             }
         }
         System.out.println(this.academicPostLinks);
-
+        System.out.println(this.academicPostLinks.size());
     }
 
 
-}
