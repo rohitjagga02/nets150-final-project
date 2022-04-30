eae6df292c8e381ddd2a05a9cc52a48702a2c462
Rohit Jagga <rohitjagga02@gmail.com>
2022-04-30 14:41:52 -0400

    base code for parser

---
+++ src/hw3Main.java
@@ -0,0 +1,16 @@
+public class hw3Main {
+    public static void main(String[] args) {
+        ciaParser cparse = new ciaParser();
+
+        // Comment out the questions you are not currently viewing, to improve the program's execution speed.
+        // Change the question's parameters to evaluate that question for different regions/continents/colors/etc.
+        cparse.questionOne("red","white");
+        cparse.questionTwo("Afghanistan");
+        cparse.questionThree("Africa");
+        cparse.questionFour("Europe");
+        cparse.questionFive("South America");
+        cparse.questionSix("Caribbean");
+        cparse.questionSeven("D");
+        cparse.questionEight("Europe");
+    }
+}
